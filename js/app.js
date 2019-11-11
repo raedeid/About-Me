af1252e7be263d76401c88a8b105760972e03c9d
Rashid-Alsawalqa <rashid.dmcr@gmail.com>
2019-11-11 21:57:59 +0200

    Update 1.2v

---
+++ js/app.js
@@ -0,0 +1,63 @@
+'use strict'
+
+var user = prompt('wht\'s your name');
+
+alert('Welcome to my website ' + user );
+alert('Please answer my question by type Yes or No')
+
+
+
+var name 
+name = prompt ('My name Rashid Saqr Musa Alsawalqa ?? ');
+name = name.toUpperCase(); 
+if (name == 'YES') {
+    alert('Burfict answer + 1')
+  } else if (name == 'NO') {
+    alert('wrong answer + 0')
+  }
+  //console.log(name);
+
+  var age 
+  age = prompt ('I\'am 30 years old ?? ');
+  age = age.toUpperCase();
+if (age == 'YES') {
+    alert('Wrong answer + 0')
+  } else if (age == 'NO') {
+    alert('Burfict answer + 1')
+  }
+  //console.log(age);
+
+  var student  
+  student  = prompt ('I still a student in mutah university ?? ');
+  student = student.toUpperCase();
+if (student  == 'YES') {
+    alert('Wrong answer + 0')
+  } else if (student  == 'NO') {
+    alert('Burfict answer + 1')
+  }
+  //console.log(student);
+
+  var game  
+  game  = prompt (' Did i Love VideoGames ??? ');
+  game = game.toUpperCase();
+if (game  == 'YES') {
+    alert('Burfict answer + 1')
+  } else if (game  == 'NO') {
+    alert('Wrong answer + 0')
+  }
+  //console.log(game);
+
+  var JavaScript  
+  JavaScript  = prompt (' Did i love JavaScript ??? ');
+  JavaScript = JavaScript.toUpperCase();
+if (JavaScript  == 'YES') {
+    alert('Wrong answer + 0')
+  } else if (JavaScript  == 'NO') {
+    alert('Burfict answer + 1')
+  }
+  //console.log(JavaScript);
+  
+
+  alert(' Thank you ' + user );
+
+
